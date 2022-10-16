1d8b1a7272131f2b5f45d3df65f8aadeb525c2bb
Abbas Salama <abbassalama2@gmail.com>
2022-10-16 14:00:27 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,53 +1,45 @@
-var selections = ['rock', 'paper', 'scissors'];
-var computerSelection = "";
-var playerSelection = prompt();
-
 function computerPlay(){
-    selection = Math.floor(Math.random() * selections.length);
+    let selections = ['rock', 'paper', 'scissors'];
+    let selection = Math.floor(Math.random() * selections.length);
     computerSelection = selections [selection]
     return computerSelection
 }
 
-computerPlay();
-
 function playRound(playerSelection, computerSelection){
-    if (playerSelection == 'rock' && computerSelection == 'rock'){
-        return 'draw'
+    if (playerSelection == computerSelection){
+        return 'draw';
     }
     else if(playerSelection == 'rock' && computerSelection == 'paper'){
-        return 'paper wins'
+        return 'You Lose! Paper beats Rock';
     }
     else if(playerSelection == 'rock' && computerSelection == 'scissors'){
-        return 'rock wins'
+        return 'You Win! Rock beats Scissors';
     }
     else if(playerSelection == 'paper' && computerSelection == 'rock'){
-        return 'paper wins'
-    }
-    else if(playerSelection == 'paper' && computerSelection == 'paper'){
-        return 'draw'
+        return 'You Win! Paper beats Rock';
     }
     else if(playerSelection == 'paper' && computerSelection == 'scissors'){
-        return 'scissors wins'
+        return 'You Lose! Scissors beats Paper';
     }
     else if(playerSelection == 'scissors' && computerSelection == 'rock'){
-        return 'rock wins'
+        return 'You Lose! Rock beats Scissors';
     }
     else if(playerSelection == 'scissors' && computerSelection == 'paper'){
-        return 'scissors wins'
-    }
-    else if(playerSelection == 'scissors' && computerSelection == 'scissors'){
-        return 'draw'
+        return 'You Win! Scissors beats Paper';
     }
     else{
-        return 'Please enter either rock, paper or scissors.'
+        return 'Please enter either rock, paper or scissors.';
     }
 }
 
-function game(){
-    for (let i=0; i<5; i++){
-        playRound();
+function play(){
+    for (let i = 0; i < 5; i++){
+        playerSelection = window.prompt().toLowerCase();
+        computerSelection = computerPlay();
+        console.log("you selected", playerSelection);
+        console.log(computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
     }
 }
 
-console.log(computerSelection);
-console.log(playRound(computerSelection, playerSelection));
+play();
